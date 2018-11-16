1a148e1833ee3beed258c4800f38ee0147a53a87
edwin charles <charlymk56@gmail.com>
2018-11-16 22:12:47 +0530

    ...

--- www/storekeeper.php
+++ www/storekeeper.php
@@ -15,47 +15,75 @@
                 <li><a href="index.php" name="logout">Logout</a></li>
             </ul>
         </nav>
-        <div id="middle">
-            <div id="details">
-                <div class="output">
-                    <label>#ID</label>
-                    <input type="integer" name="barcode" autofocus="" maxlength="8" required="">
-                </div>
+        <div id="middle_left">
+            <div class="output">
+                <label>#ID</label>
+                <input type="integer" name="barcode" autofocus="" maxlength="8" required="">
                 <?php
-                    if(isset($_POST['barcode'])){
+                    if(isset($_POST['enter'])){
                         $code=$_POST['barcode'];
+                        echo $code;
                         //CREATE QUERY
                         $query="SELECT NAME,UNIT_PRICE FROM item where ID=$code;";
                         //GET RESULT
                         $result=mysqli_query($conn,$query);
                         //FETCH DATA 
                         $details=mysqli_fetch_all($result,MYSQLI_ASSOC);
-                        echo $details;
                         //FREE RESULT
-                        mysqli_free_result($result);
+                        mysqli_free_result($result);  
+                        
+                        $_POST['name']=$details['NAME'];
+                        $_POST['unitprice']=$details['UNIT_PRICE'];
+                        echo $_POST['name'];
+                        echo $_POST['unitprice'];
+                    } r
                 ?>
-                <script type="text/javascript" src="functions.js"></script>
-                <?php        
-                    }
-                ?>
-                <div class="output">
-                    <label>NAME</label>
-                    <input type="text" name="name" value="<?php echo $details['NAME'];?>" disabled="">
-                </div>
-                <div class="output">
-                    <label>QUANTITY</label>
-                    <input type="number" name="qty" required="">
-                </div>
-                <div class="output">
-                    <label>UNIT PRICE</label>
-                    <input type="integer" name="unitprice" value="<?php echo $details['UNIT_PRICE'];?>" disabled="">
-                </div>
-                <?php     
-                    //CLOSE CONNECTION 
-                    mysqli_close($conn);
-                ?>
-            </div>
+                <input type="submit" name="enter" value="Enter">
+            </div>  
         </div>
+        <?php
+            if(isset($_POST['enter'])){
+                $code=$_POST['barcode'];
+                echo $code;
+                //CREATE QUERY
+                $query="SELECT NAME,UNIT_PRICE FROM item where ID=$code;";
+                //GET RESULT
+                $result=mysqli_query($conn,$query);
+                //FETCH DATA 
+                $details=mysqli_fetch_all($result,MYSQLI_ASSOC);
+                //FREE RESULT
+                mysqli_free_result($result);  
+                
+                $_POST['name']=$details['NAME'];
+                $_POST['unitprice']=$details['UNIT_PRICE'];
+                echo $_POST['name'];
+                echo $_POST['unitprice'];
+            }
+        ?>
+        <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>">
+            <div id="middle">
+                <div id="details">
+                    <div class="output">
+                        <label>NAME</label>
+                        <input type="text" name="name" value="" disabled="">
+                    </div>
+                    <div class="flex_class">
+                        <div class="output">
+                            <label>QUANTITY</label>
+                            <input type="number" name="qty" required="">
+                        </div>
+                        <div class="output">
+                            <label>UNIT PRICE</label>
+                            <input type="integer" name="unitprice" value="" disabled="">
+                        </div>
+                    </div>
+                    <?php     
+                        //CLOSE CONNECTION 
+                        mysqli_close($conn);
+                    ?>
+                </div>
+            </div>
+        </form>
     </body>
     <footer>
         <p>&copy STORE MANAGEMENT SYSTEM</p>
